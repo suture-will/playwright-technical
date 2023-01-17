560c8727f60fa0a74be4695810abd999ea3e8f35
William Golden <wtgolden5@gmail.com>
2023-01-17 11:43:47 -0500

    comment cleanup

--- e2e/pokemon.spec.ts
+++ e2e/pokemon.spec.ts
@@ -1,13 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 
-// for this test: navigate to the base page and verify that the page contains certain elements
+// For this test: navigate to the base page and verify that the page contains certain elements
 test('navigate to page and verify', async ({ page }) => {
     // TODO: Go to the index page (the baseURL is set via the webServer in the playwright.config.ts)
 
     // code that here...
 
     // TODO: Test that expects the url should be "/" (baseURL is used there) if not using the base url, use the full url.
+
     // code that here...
     
     // TODO: Test that the new page contains an h1 with "Suture Health Demo" and an h2 with "Dittos Abilities:"
@@ -16,7 +17,7 @@ test('navigate to page and verify', async ({ page }) => {
   })
 
 
-// for this test: click the button and verify that the page displays the data that is fetched from the pokemon api properly
+// For this test: ensure that on button click: data is fetched and displayed properly. Verify that the page displays the ability 'limber' in a <p> tag.
   test('should fetch and display Ditto abilities', async ({ browser, page }) => {
  
     // code here...
